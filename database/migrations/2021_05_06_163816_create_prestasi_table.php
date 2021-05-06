c60c90149da3b265c89525c5de4a0f64d8168abb
jerry2204 <jerryandrianto22@gmail.com>
2021-05-06 23:43:08 +0700

    make prestasi migration

---
+++ database/migrations/2021_05_06_163816_create_prestasi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrestasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prestasi', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('program_studi');
+            $table->string('prestasi');
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestasi');
+    }
+}
