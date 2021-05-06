869dcb80a884d5630cf71a247eaa247871a1c586
jerry2204 <jerryandrianto22@gmail.com>
2021-05-06 23:48:24 +0700

    make prestasi model

---
+++ app/Models/Prestasi.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prestasi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'prestasi';
+    protected $fillable = ['name', 'program_studi', 'prestasi', 'tanggal'];
+}
