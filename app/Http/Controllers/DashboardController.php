5c6569aeecbed43c40cecee3b833a93ad8eeb15c
jerry2204 <jerryandrianto22@gmail.com>
2021-05-26 16:34:38 +0700

    finishing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AnggotaDepartemen;
+use App\Models\BPH;
+use App\Models\Departemen;
+use App\Models\Kadep;
+use App\Models\Pemasukan;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('admin.index');
+        $bph = BPH::count();
+        $kadep = Kadep::count();
+        $anggota = AnggotaDepartemen::count();
+        $saldo = Pemasukan::saldo();
+        $departemen = Departemen::all();
+
+        return view('admin.index', compact('bph', 'kadep', 'anggota', 'saldo', 'departemen'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,10 +10,62 @@
         </div>
         <div class="col-md-8">
             <h4 class="font-20 weight-500 mb-10 text-capitalize">
-                Welcome back <div class="weight-600 font-30 text-blue">@if (auth()->user()) {{ auth()->user()->name }} @else John Doe @endif</div>
+                Selamat Datang <div class="weight-600 font-30 text-blue">@if (auth()->user()) {{ auth()->user()->name }} @else John Doe @endif</div>
             </h4>
-            <p class="font-18 max-width-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde hic non repellendus debitis iure, doloremque assumenda. Autem modi, corrupti, nobis ea iure fugiat, veniam non quaerat mollitia animi error corporis.</p>
+            <p class="font-18 max-width-600 text-justify">Selamat melakukan tugas dengan baik pada Sistem Informasi Badan Eksekutif Mahasiswa Institut Teknologi Del, Sistem Informasi Badan Eksekutif Mahasiswa Institut Teknologi Del ini dibuat untuk membantu pengurus Badan Eksekutif Mahasiswa dalam melakukan pengelolaan data pengurus, keuangan, serta membantu masyarakat umum untuk mengenal Badan Eksekutif Mahasiswa Institut Teknologi Del.</p>
         </div>
     </div>
+</div>
+<div class="row">
+    <div class="col-xl-4 col-6 mb-30">
+        <div class="card-box height-100-p widget-style1">
+            <div class="text-center">
+                <div class="h4 mb-0">Rp. {{ number_format($saldo, 2, ',', '.') }}</div>
+                <div class="weight-600 font-14">Saldo BEM</div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-4 col-6 mb-30">
+        <div class="card-box height-100-p widget-style1">
+            <div class="text-center">
+                <div class="h4 mb-0">{{ $bph }}</div>
+                <div class="weight-600 font-14">Badan Pengurus Harian</div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-4 col-6 mb-30">
+        <div class="card-box height-100-p widget-style1">
+            <div class="text-center">
+                <div class="h4 mb-0">{{ $kadep }}</div>
+                <div class="weight-600 font-14">Kepala Departemen</div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-4 col-6 mb-30">
+        <div class="card-box height-100-p widget-style1">
+            <div class="text-center">
+                <div class="h4 mb-0">{{ $anggota }}</div>
+                <div class="weight-600 font-14">Anggota</div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-4 col-6 mb-30">
+        <div class="card-box height-100-p widget-style1">
+            <div class="text-center">
+                <div class="h4 mb-0">{{ $departemen->count() }}</div>
+                <div class="weight-600 font-14">Departemen</div>
+            </div>
+        </div>
+    </div>
+    @foreach ($departemen as $item)
+    <div class="col-xl-4 col-6 mb-30">
+        <div class="card-box height-100-p widget-style1">
+            <div class="text-center">
+                <div class="h4 mb-0">{{ $item->anggota_departemen->count() }}</div>
+                <div class="weight-600 font-14">Anggota {{ $item->name }}</div>
+            </div>
+        </div>
+    </div>
+    @endforeach
 </div>
 @endsection
